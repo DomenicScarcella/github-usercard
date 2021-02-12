9bf76bf3bda759e916ccb3ebc11aa4ba1d32982f
Domenic Scarcella <ls41513@yahoo.com>
2021-02-11 23:32:18 -0500

    MVP done!

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,8 +3,6 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-// axios.get('https://api.github.com/users/DomenicScarcella')
-
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -19,8 +17,6 @@
     and append the returned markup to the DOM as a child of .cards
 */
 
-const allCards = document.querySelector('.cards');
-
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
@@ -31,14 +27,6 @@ const allCards = document.querySelector('.cards');
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
 */
-
-const followersArray = [
-  'tetondan',
-  'dustinmyers',
-  'justsml',
-  'luishrd',
-  'bigknell'
-];
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -59,6 +47,7 @@ const followersArray = [
       </div>
     </div>
 */
+
 function userCardMaker (object) {
   const divCard = document.createElement('div')
   const imgUser = document.createElement('img')
@@ -77,16 +66,6 @@ function userCardMaker (object) {
   h3Name.classList.add('name')
   pUsername.classList.add('username')
 
-  imgUser.src = object.avatar_url
-  h3Name.textContent = object.h3Name
-  pUsername.textContent = object.login
-  pLoc.textContent = `Location: ${object.location}`
-  pProf.textContent = `Profile: ${aProfLink}`
-  aProfLink.href = object.html_url
-  pFollowers.textContent = `Followers: ${object.followers}`
-  pFollowing.textContent = `Following: ${object.following}`
-  pBio.textContent = `Bio: ${object.bio}`
-
   divCard.appendChild(imgUser)
   divCard.appendChild(divInfo)
   divInfo.appendChild(h3Name)
@@ -96,21 +75,45 @@ function userCardMaker (object) {
   divInfo.appendChild(pFollowers)
   divInfo.appendChild(pFollowing)
   divInfo.appendChild(pBio)
+
+  imgUser.src = object.avatar_url
+  h3Name.textContent = object.name
+  pUsername.textContent = object.login
+  pLoc.textContent = `Location: ${object.location}`
+  pProf.textContent = `Profile: `
+  aProfLink.href = object.html_url
+  aProfLink.textContent = object.html_url
   pProf.appendChild(aProfLink)
+  pFollowers.textContent = `Followers: ${object.followers}`
+  pFollowing.textContent = `Following: ${object.following}`
+  pBio.textContent = `Bio: ${object.bio}`
 
   return divCard
-  
 }
 
-const cardElements = followersArray.map(cardObj => {
-  const divCard = userCardMaker(cardObj)
-  return divCard
-})
-cardElements.forEach(divCard => {
-  allCards.appendChild(divCard)
-})
+const allCards = document.querySelector('.cards');
 
+axios.get('https://api.github.com/users/DomenicScarcella')
+  .then(res => {
+    const divCard = userCardMaker(res.data);
+    allCards.appendChild(divCard);
+  
+  })
+  .catch(err =>{
+    console.log(err)
+  })
 
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+followersArray.forEach((item) => {
+  axios.get(`https://api.github.com/users/${item}`)
+  .then((item) => {
+    const divCard = userCardMaker(item.data)
+    allCards.appendChild(divCard);
+  })
+  .catch(err =>{
+    console.log(err)
+  })
+})
 
 /*
   List of LS Instructors Github username's:
